dbe0400dc4374c631e299ede77f8fbedd46c9279
kimya-h <kimya.mailbox@gmail.com>
2024-01-25 15:45:05 +0330

    adding script

---
+++ script.js
@@ -0,0 +1,31 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragStart', dragStart)
+fill.addEventListener('dragEnd', dragEnd)
+
+function dragStart() {
+    console.log('drag start')
+}
+
+function dragEnd() {
+    console.log('drag end')
+}
+
+function dragOver() {
+    console.log('drag over')
+}
+
+function dragEnter() {
+    console.log('drag enter')
+}
+
+function dragLeave() {
+    console.log('drag leave')
+}
+
+function dragDrop() {
+    console.log('drag drop')
+}
+
+ 
